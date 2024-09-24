06d9759869e77a3b3031717c957f85dc08bd7392
Karmatack <kennedy31agost@gmail.com>
2024-09-24 15:47:23 -0500

    update

--- Cursos/global/js/scripts.js
+++ Cursos/global/js/scripts.js
@@ -1,8 +1,12 @@
 function toggleSection(sectionId) {
     const section = document.getElementById(sectionId);
+    const icon = section.previousElementSibling.querySelector('.icon');
+    
     if (section.style.display === 'none' || section.style.display === '') {
         section.style.display = 'block';
+        icon.style.transform = 'rotate(90deg)';
     } else {
         section.style.display = 'none';
+        icon.style.transform = 'rotate(0deg)';
     }
 }
